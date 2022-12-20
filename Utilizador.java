5e83ced1815f34afa62a1067c02b35fecea10351
Tiago8067 <tiagosoarez6@gmail.com>
2022-12-20 19:42:36 +0000

    inicio

---
+++ Utilizador.java
@@ -0,0 +1,103 @@
+import java.io.Serializable;
+
+public abstract class Utilizador implements Serializable {
+    private int id;
+    private  String username;
+    private  String email;
+    private  String password;
+    private  String nome;
+    private  String genero;
+    private  String dataDeNascimento;
+    private  String codigoPostal;
+    private  String morada;
+    private TipoUtilizador tipoUtilizador;
+
+    public Utilizador() {
+    }
+
+    public Utilizador(String username, TipoUtilizador tipoUtilizador) {
+        this.username = username;
+        this.tipoUtilizador = tipoUtilizador;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public TipoUtilizador getTipoUtilizador() {
+        return tipoUtilizador;
+    }
+
+    public void setTipoUtilizador(TipoUtilizador tipoUtilizador) {
+        this.tipoUtilizador = tipoUtilizador;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getDataDeNascimento() {
+        return dataDeNascimento;
+    }
+
+    public void setDataDeNascimento(String dataDeNascimento) {
+        this.dataDeNascimento = dataDeNascimento;
+    }
+
+    public String getCodigoPostal() {
+        return codigoPostal;
+    }
+
+    public void setCodigoPostal(String codigoPostal) {
+        this.codigoPostal = codigoPostal;
+    }
+
+    public String getMorada() {
+        return morada;
+    }
+
+    public void setMorada(String morada) {
+        this.morada = morada;
+    }
+
+}
