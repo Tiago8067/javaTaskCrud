fdd2b86066ab0a5dd40d3985cdaed1cd5cc9bab8
basiliobarbosaESTG <basiliobarbosa@ipvc.pt>
2022-12-21 21:43:12 +0000

    teste

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ import menus.*;
 
 public class Main {
     public static void main(String[] args) {
+        // SAI DEL SOL
         Database db = new Database();
         db.criaFicheiroSeNaoExistir();
 
